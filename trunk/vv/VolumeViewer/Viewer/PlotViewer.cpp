a5b7c6acb30a028428128acf28f3dafb900d0f15
jimpelton@u.boisestate.edu <jimpelton@u.boisestate.edu@75636ee4-a319-ddac-ad8d-1044cd092777>
2012-01-02 05:32:27 +0000

    PlotViewer render multiple plots on single chart.

--- trunk/vv/VolumeViewer/Viewer/PlotViewer.cpp
+++ trunk/vv/VolumeViewer/Viewer/PlotViewer.cpp
@@ -12,29 +12,34 @@
 #include <map>
 
 PlotViewer::PlotViewer(DataReaderFormat drf,int screenWidth, int screenHeight) :
-	Viewer(drf, screenWidth, screenHeight), m_numComponents(0), m_chartType(0) {}
-
-PlotViewer::PlotViewer(DataReaderFormat drf, int screenWidth, int screenHeight,
-		vtkSmartPointer<vtkDataArray> xarr, vtkSmartPointer<vtkDataArray> yarr) :
-		Viewer(drf, screenWidth, screenHeight), m_numComponents(0), m_chartType(0)
+	Viewer(drf, screenWidth, screenHeight), m_numComponents(0)
 {
-		m_arrX = xarr;
-		m_arrY = yarr;
+	//allComponentsSet = 0x00;
 }
 
+//PlotViewer::PlotViewer(DataReaderFormat drf, int screenWidth, int screenHeight,
+//		vtkSmartPointer<vtkDataArray> xarr, vtkSmartPointer<vtkDataArray> yarr) :
+//		Viewer(drf, screenWidth, screenHeight), m_numComponents(0), m_chartType(0)
+//{
+//		m_arrX = xarr;
+//
+//}
 
-PlotViewer::~PlotViewer() {}
 
+PlotViewer::~PlotViewer()
+{}
 
+/**
+ * Add all the columns, check the sizes, set number of rows.
+ * @return
+ */
 int PlotViewer::Setup()
 {
 	view = vtkSmartPointer<vtkContextView>::New();
 	table = vtkSmartPointer<vtkTable>::New();
-	title = vtkSmartPointer<vtkTextWidget>::New();
 
 
-
-	if (m_arrX == NULL || m_arrY == NULL){
+	if (m_arrX == NULL || m_yArrays.size() == 0){
 		fprintf(stdout, "PlotViewer::Setup(): Arrays where null, you should call SetXArray()/SetYArray() before Setup().\n");
 		return 0;
 	}
@@ -43,11 +48,14 @@ int PlotViewer::Setup()
 		fprintf(stdout, "PlotViewer::Setup(): Please set the number of components by calling PlotViewer::SetNumberOfComponents(int).\n");
 	}
 
-	m_arrX->SetName(m_nameX);
-	m_arrY->SetName(m_nameY);
-
 	table->AddColumn(m_arrX);
-	table->AddColumn(m_arrY);
+
+	std::vector<vtkSmartPointer<vtkDataArray> >::iterator iter;
+	for (iter= m_yArrays.begin(); iter != m_yArrays.end(); ++iter)
+	{
+		table->AddColumn(*iter);
+	}
+
 	table->SetNumberOfRows(m_numComponents);
 
 	return 1;
@@ -56,7 +64,7 @@ int PlotViewer::Setup()
 
 void PlotViewer::InitializeRenderer()
 {
-	if (m_arrX->GetNumberOfComponents() == 0 || m_arrY->GetNumberOfComponents() == 0){
+	if (m_arrX->GetNumberOfComponents() == 0){
 		fprintf(stdout, "PlotViewer::InitializeRenderer(): Array components are 0 length, you should add data to the table before calling this method\n");
 		return;
 	}
@@ -64,11 +72,15 @@ void PlotViewer::InitializeRenderer()
 	table->Update();
 	chart = vtkSmartPointer<vtkChartXY>::New();
 	view->GetScene()->AddItem(chart);
-	vtkPlot *plot1 = chart->AddPlot(m_chartType);
-	plot1->SetInput(table, 0, 1);
-	plot1->SetColor(0, 255, 0, 255);
-	plot1->SetWidth(1.0);
 
+	for (size_t i = 0; i < m_plots.size(); i++)
+	{
+		plot p = m_plots.at(i);
+		vtkPlot *plot1 = chart->AddPlot(p.type);
+		plot1->SetInput(table, 0, i+1);
+		plot1->SetColor(p.r, p.g, p.b, p.a);
+		plot1->SetWidth(1.0);
+	}
 	view->Update();
 }
 
@@ -84,37 +96,40 @@ vtkSmartPointer<vtkTable> PlotViewer::GetTable() const
 	return table;
 }
 
-void PlotViewer::SetXArray(vtkSmartPointer<vtkDataArray> arrX)
+void PlotViewer::SetXArray(vtkSmartPointer<vtkDataArray> arrX, char *arrName)
 {
 	m_arrX = arrX;
+	m_arrX->SetName(arrName);
 }
 
-void PlotViewer::SetYArray(vtkSmartPointer<vtkDataArray> arrY)
+/**
+ * Add an array for Y-Axis set of values to the chart.
+ * @param arrY
+ */
+void PlotViewer::AddYArray(vtkSmartPointer<vtkDataArray> arrY, char *arrName, plot *p)
 {
-	m_arrY = arrY;
+	arrY->SetName(arrName);
+	m_yArrays.push_back(arrY);
+	m_plots.push_back(*p);
 }
 
 void PlotViewer::SetNumComponents(int n)
 {
 	m_numComponents = n;
+	//allComponentsSet |= NUM_COMPONENTS_SET;
 }
 
+
+/**
+ * Add a value that will be plotted.
+ * @param row
+ * @param col
+ * @param val
+ */
 void PlotViewer::SetRowValue(vtkIdType row, vtkIdType col, vtkVariant val)
 {
 	table->SetValue(row, col, val);
+	//allComponentsSet |= ROW_VALUE_SET;
 }
 
-void PlotViewer::SetXName(char *str)
-{
-	m_nameX = str;
-}
 
-void PlotViewer::SetYName(char *str)
-{
-	m_nameY = str;
-}
-
-void PlotViewer::SetChartType(int type)
-{
-	m_chartType = type;
-}
